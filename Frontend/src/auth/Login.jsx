290b9a92c5308e20ed8762c95340baede52d13ea
Vijay <155476282+Vijayendar17@users.noreply.github.com>
2024-09-28 02:31:43 +0530

    Login.jsx

--- Frontend/src/auth/Login.jsx
+++ Frontend/src/auth/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
     setMessage(''); 
     try {
-      const response = await axios.post('http://localhost:3000/ai/login', { email, password });
+      const response = await axios.post('https://geneartive-ai.onrender.com', { email, password });
       
       if (response.status === 200) {
         localStorage.setItem('token', response.data.token);
