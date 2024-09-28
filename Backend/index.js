6d348eee6f7b5551a499f819d73cb86abc9e348d
Vijay <155476282+Vijayendar17@users.noreply.github.com>
2024-09-28 18:54:03 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,10 +10,48 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'import express from 'express'
+import cors from 'cors'
+import Connect from './config/index.js';
+import dotenv from 'dotenv';
+import router from './auth/auth.js';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+Connect()
+const app = express()
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: 'import express from 'express'
+import cors from 'cors'
+import Connect from './config/index.js';
+import dotenv from 'dotenv';
+import router from './auth/auth.js';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+Connect()
+const app = express()
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: 'https://geneartiveai.onrender.com/', 
    credentials: true,
   methods: 'GET,POST,PUT,DELETE', 
 }));
+
+app.use("/ai",router)
+
+let Port = process.env.Port || 3000;
+
+app.listen(Port, () => console.log('Server started on port 3000'));', 
+   credentials: true,
+  methods: 'GET,POST,PUT,DELETE', 
+}));
+
+app.use("/ai",router)
+
+let Port = process.env.Port || 3000;
+
 
 app.use("/ai",router)
 
