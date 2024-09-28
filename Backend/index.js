26249fd2918c96e08777788c35f8efa754b0c4cf
Vijay <155476282+Vijayendar17@users.noreply.github.com>
2024-09-28 18:58:23 +0530

    index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,28 +1,3 @@
-import express from 'express'
-import cors from 'cors'
-import Connect from './config/index.js';
-import dotenv from 'dotenv';
-import router from './auth/auth.js';
-import cookieParser from 'cookie-parser';
-dotenv.config();
-Connect()
-const app = express()
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-  origin: 'import express from 'express'
-import cors from 'cors'
-import Connect from './config/index.js';
-import dotenv from 'dotenv';
-import router from './auth/auth.js';
-import cookieParser from 'cookie-parser';
-dotenv.config();
-Connect()
-const app = express()
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({
-  origin: 'import express from 'express'
 import cors from 'cors'
 import Connect from './config/index.js';
 import dotenv from 'dotenv';
