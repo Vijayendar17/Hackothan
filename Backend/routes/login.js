e3851aa4af5f60c0036083ff5cbbfae2c8765cf9
Vijay <155476282+Vijayendar17@users.noreply.github.com>
2024-09-28 19:15:40 +0530

    login.js

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -1,31 +1,34 @@
 import jwt from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs';
+import UserModel from '../models/UserModel.js'; 
 
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
- 
-
     const user = await UserModel.findOne({ email });
+    if (!user) return res.status(401).send({ message: "Invalid email or password" });
+
+
     const isMatch = await bcryptjs.compare(password, user.password);
-    
     if (!isMatch) return res.status(401).send({ message: "Invalid email or password" });
 
 
-    const token = jwt.sign({ name: user.name, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ name: user.name, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1d' });
 
 
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', 
-      maxAge: 36000000,
+      maxAge: 3600000, 
       sameSite: 'Strict',
     });
 
-    return res.status(200).send({ message: "Logged in successfully" });
+    return res.status(200).send({ message: "Logged in successfully", token });
   } catch (error) {
     console.error("Error during login:", error);
     return res.status(500).send({ message: "Server error" });
   }
 };
+
 
